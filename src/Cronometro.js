1d69395538e4f11808b6c1d47aa476d26b7c687e
AndersonAPinto <andersonaugustopinto@gmail.com>
2024-02-29 10:58:13 -0300

    update Cronometro

--- src/Cronometro.js
+++ src/Cronometro.js
@@ -3,10 +3,10 @@ import { View, Text, Button, Alert } from 'react-native';
 import HistoricoEventos from './HistoricoEventos';
 import { useNavigation } from '@react-navigation/native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import { Audio } from 'expo-av';
 
 
-
- function Cronometro ({ cargaHorariaFormatada }) {
+function Cronometro({ cargaHorariaFormatada }) {
     const navigation = useNavigation();
     const [isRunning, setIsRunning] = useState(false);
     const [tempoDecorrido, setTempoDecorrido] = useState(0);
@@ -20,7 +20,15 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
     const minutosDecorridos = Math.floor((tempoDecorrido % 3600) / 60);
     const segundosDecorridos = tempoDecorrido % 60;
     const tempoDecorridoFormatado = `${horasDecorridas}h e ${minutosDecorridos}min ${segundosDecorridos}s`;
-   
+
+    async function playAudio() {
+        const { sound } = await Audio.Sound.createAsync(
+            require('../src/music/AlarmSlow.mp3'), // Substitua pela URL do seu áudio
+            { shouldPlay: true }
+        );
+        await sound.playAsync();
+        return sound;
+    }
 
     useEffect(() => {
         if (isRunning) {
@@ -31,13 +39,18 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
                     const cargaTotalMinutos = horas * 60 + minutos;
 
                     // Verifica para pausas necessárias a cada 4 horas
-                    if (novoTempo % 240 === 0) { // A cada 240 "minutos" (neste exemplo, segundos simulando minutos)
+                    if (novoTempo % 14400 === 0) { // A cada 240 "minutos" (neste exemplo, segundos simulando minutos)
                         clearInterval(id);
                         setIsRunning(false);
-                        Alert.alert('Pausa Necessária', 'Faça uma pausa de no mínimo 15 minutos.');
+                        playAudio().then(sound => {
+                            Alert.alert('Pausa Necessária', 'Faça uma pausa de no mínimo 15 minutos.', [
+                                { text: "OK", onPress: () => sound.stopAsync() } // Para a reprodução ao tocar em OK
+                            ]);
+                        });
+                        //Alert.alert('Pausa Necessária', 'Faça uma pausa de no mínimo 15 minutos.');
                     }
                     // Verifica se o tempo decorrido atingiu a carga total de 9h:50min
-                    if (novoTempo === 590) {
+                    if (novoTempo === 35400){
                         clearInterval(id); // Para o cronômetro
                         setIsRunning(false); // Atualiza o estado para pausar o cronômetro
                         // Exibe o alerta de carga horária completada
@@ -45,14 +58,14 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
                         return prevTempo; // Retorna o tempo atual sem incrementar, pois atingiu a carga total
                     }
                     // Verifica se o tempo decorrido atingiu a carga total de minutos
-                    if (novoTempo === cargaTotalMinutos) {
+                    if (novoTempo === cargaTotalMinutos * 60) {
                         clearInterval(id); // Para o cronômetro
                         setIsRunning(false); // Atualiza o estado para pausar o cronômetro
                         Alert.alert('Tempo Concluído', 'Você concluiu a carga horária planejada.');
                     }
                     return novoTempo; // Incrementa o tempo decorrido
                 });
-            }, 1000); // 60000 ms = 1 minuto
+            }, 1); // 60000 ms = 1 minuto
             setIntervalId(id); // Salva o ID do intervalo para poder limpar depois
             return () => clearInterval(id); // Limpa o intervalo ao desmontar o componente ou quando pausar
         }
@@ -64,17 +77,19 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
             intervalId = setInterval(() => {
                 const agora = new Date();
                 const diferencaHora = agora.getTime() - new Date(UltimoStop).getTime();
+                const tempoDePausa = 39600000; // 11 horas de Pausa
 
-                if (diferencaHora < 10000) {
-                    const horasRestantes = Math.floor((39600000 - diferencaHora) / 3600000);
-                    const minutosRestantes = Math.floor(((39600000 - diferencaHora) % 3600000) / 60000);
+                if (diferencaHora < tempoDePausa){
+                    const horasRestantes = Math.floor((tempoDePausa - diferencaHora) / 3600000);
+                    const minutosRestantes = Math.floor(((tempoDePausa - diferencaHora) % 3600000) / 60000);
+                    const segundosRestantes = Math.floor(((tempoDePausa - diferencaHora) % 60000) / 1000);
 
-                    setMensagemTempoRestante(`Aguarde ${horasRestantes}h e ${minutosRestantes}min para reiniciar o cronômetro.`);
+                    setMensagemTempoRestante(`Aguarde ${horasRestantes}h ${minutosRestantes}min ${segundosRestantes}s para reiniciar o cronômetro.`);
                 } else {
                     setMensagemTempoRestante("");
                     clearInterval(intervalId);
                 }
-            }, 1000); //1000ms === 1 segundo ou 60000 === 1 minuto
+            },10000); //1000ms === 1 segundo ou 60000 === 1 minuto
         }
 
         return () => clearInterval(intervalId);
@@ -105,14 +120,14 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
         setHistoricoStop(true);
         setHistoricoEventos(historicoAtual => [...historicoAtual, { tipo: "Stop", momento: new Date() }]);
         const eventoStop = { tipo: "Stop", momento: new Date().toLocaleDateString() };
-        await salvarEvento(eventoStop); 
+        await salvarEvento(eventoStop);
         const dataSalvamento = new Date();
         const eventosSalvos = historicoEventos.filter(evento => evento.tipo === "Play" || evento.tipo === "Pause");
         try {
             const dadosAtuais = await AsyncStorage.getItem('dadosSalvos');
             const dadosAtualizados = dadosAtuais ? JSON.parse(dadosAtuais) : {};
             dadosAtualizados[dataSalvamento] = eventosSalvos;
-    
+
             await AsyncStorage.setItem('dadosSalvos', JSON.stringify(dadosAtualizados));
             Alert.alert("Carga horária do dia salva");
             navigation.navigate('Teste');
@@ -122,45 +137,46 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
         setHistoricoEventos([]);
         setTimeout(() => {
             setHistoricoStop(false);
-        }, 10000);
+        }, 1000);
     };
 
 
     const salvarEvento = async (evento) => {
         try {
-          const eventosExistentes = await AsyncStorage.getItem('eventosSalvos');
-          const eventosAtualizados = eventosExistentes ? JSON.parse(eventosExistentes) : [];
-          eventosAtualizados.push(evento);
-          await AsyncStorage.setItem('eventosSalvos', JSON.stringify(eventosAtualizados));
+            const eventosExistentes = await AsyncStorage.getItem('eventosSalvos');
+            const eventosAtualizados = eventosExistentes ? JSON.parse(eventosExistentes) : [];
+            eventosAtualizados.push(evento);
+            await AsyncStorage.setItem('eventosSalvos', JSON.stringify(eventosAtualizados));
         } catch (error) {
-          console.log("Erro ao salvar evento", error);
+            console.log("Erro ao salvar evento", error);
         }
-      };
+    };
     // Formatação do tempo decorrido para exibição
     return (
-            <View>
-                {mensagemTempoRestante ? (
-                    // Se existe uma mensagem de tempo restante, exibe ela
-                    <Text>{mensagemTempoRestante}</Text>
-                ) : (
-                    // Caso contrário, exibe o tempo decorrido
-                    <Text>Tempo Decorrido: {tempoDecorridoFormatado}</Text>
-                )}
-                {!isRunning && <Button title="Play" onPress={handlePlay} />}
-                {isRunning && <Button title="Pause" onPress={handlePause} />}
-                <Button title="Stop"
-                    disabled={historicoEventos.length === 0} 
-                    onPress={() => {
-                        handleStop();
-                        console.log(JSON.stringify(dadosSalvos));
-                        }} />
-                <HistoricoEventos historicoEventos={historicoEventos} />
-                
-                
-            </View>
-        
-    );
+        <View>
+            {mensagemTempoRestante ? (
+                // Se existe uma mensagem de tempo restante, exibe ela
+                <Text>{mensagemTempoRestante}</Text>
+            ) : (
+                // Caso contrário, exibe o tempo decorrido
+                <Text>Tempo Decorrido: {tempoDecorridoFormatado}</Text>
+            )}
+            {!isRunning && <Button title="Play" 
+                onPress={handlePlay}
+                disabled = {!!mensagemTempoRestante} //Converte a string para booleano
+                />}
+            {isRunning && <Button title="Pause" onPress={handlePause} />}
+            <Button title="Stop"
+                disabled={historicoEventos.length === 0}
+                onPress={() => {
+                    handleStop();
+                    console.log(JSON.stringify(dadosSalvos));
+                }} />
+            <HistoricoEventos historicoEventos={historicoEventos} />
 
+        </View>
+
+    );
 };
 export default Cronometro
 
--- src/TemplateTeste.js
+++ src/TemplateTeste.js
@@ -1,11 +1,37 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { View, TextInput, StyleSheet } from 'react-native';
 import CalculadoraDiasUteis from './CalculadoraDiasUteis'
 import { SafeAreaView } from 'react-native-safe-area-context';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
 
 function TemplateTeste() {
+
     const [cargaHoraria, setCargaHoraria] = useState(0);
-    console.log('cargaHoraria antes de passar para CalculadoraDiasUteis:', cargaHoraria);
+    //console.log('cargaHoraria antes de passar para CalculadoraDiasUteis:', cargaHoraria);
+    const salvarCargaHoraria = async (valor) => {
+        try {
+          await AsyncStorage.setItem('@cargaHoraria', valor.toString());
+        } catch (e) {
+          console.error("Erro ao salvar a carga horária", e);// salvar erro
+        }
+      };
+
+      useEffect(() => {
+        const carregarCargaHoraria = async () => {
+          try {
+            const valor = await AsyncStorage.getItem('@cargaHoraria');
+            if (valor !== null) {
+              // O valor existe, atualiza o estado
+              setCargaHoraria(parseInt(valor));
+            }
+          } catch (e) {
+            // erro ao ler o valor
+            console.error("Erro ao carregar a carga horária", e);
+          }
+        };
+        carregarCargaHoraria();
+      }, []);
 
     return (
         <SafeAreaView style={styles.container}>
@@ -16,7 +42,11 @@ function TemplateTeste() {
                     selectionColor="transparent"
                     placeholder="Digite a carga horária total"
                     value={cargaHoraria.toString()}
-                    onChangeText={text => setCargaHoraria(parseInt(text) || 0)}
+                    onChangeText={text => {
+                        const valorNumerico = parseInt(text) || 0;
+                        setCargaHoraria(valorNumerico);
+                        salvarCargaHoraria(valorNumerico); // Salva no AsyncStorage
+                      }}
                     keyboardType="numeric"
                 />
                 <CalculadoraDiasUteis cargaHoraria={cargaHoraria} />
