dbd2e84d702e1254dc548bf653680a80d0b0475c
AndersonAPinto <andersonaugustopinto@gmail.com>
2024-03-01 00:49:49 -0300

    update Historico

--- src/DadosSalvos.js
+++ src/DadosSalvos.js
@@ -2,6 +2,7 @@ import React, { useState, useCallback } from 'react';
 import { View, Text, ScrollView, StyleSheet, TouchableWithoutFeedback, Alert } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { useFocusEffect } from '@react-navigation/native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 
 const DadosSalvos = () => {
@@ -43,52 +44,61 @@ const DadosSalvos = () => {
     const datasSalvamento = Object.keys(dadosSalvos);
 
     return (
+
         <ScrollView style={styles.container}>
-            {datasSalvamento.length > 0 ? datasSalvamento.reverse().map((data, index) => {
-                return (
-                    <TouchableWithoutFeedback
-                        key={index}
-                        onPressIn={() => onLongPress(data)}
-                        onPressOut={onPressOut}
-                    >
-                        <View style={styles.dataContainer}>
-                            <Text style={styles.dataTitle}>Data: {data}</Text>
-                            {dadosSalvos[data].map((evento, idx) => (
-                                <Text key={idx} style={styles.eventoText}>
-                                    {evento.tipo} às {new Date(evento.momento).toLocaleTimeString()}
-                                </Text>
-                            ))}
-                        </View>
-                    </TouchableWithoutFeedback>
-                );
-            }) : (
-                <View style={styles.noDataContainer}>
-                  <Text>Sem dados salvos!</Text>
-                </View>
-            )}
+            <SafeAreaView>
+                {datasSalvamento.length > 0 ? datasSalvamento.reverse().map((data, index) => {
+                    return (
+                        <TouchableWithoutFeedback
+                            key={index}
+                            onPressIn={() => onLongPress(data)}
+                            onPressOut={onPressOut}
+                        >
+                            <View style={styles.dataContainer}>
+                                <Text style={styles.dataTitle}>Data: {data}</Text>
+                                {dadosSalvos[data].map((evento, idx) => (
+                                    <Text key={idx} style={styles.eventoText}>
+                                        {evento.tipo} às {new Date(evento.momento).toLocaleTimeString()}
+                                    </Text>
+                                ))}
+                            </View>
+                        </TouchableWithoutFeedback>
+                    );
+                }) : (
+                    <View >
+                        <Text style={styles.noDataContainer}>Sem dados salvos!</Text>
+                    </View>
+                )}
+            </SafeAreaView>
         </ScrollView>
+
+
     );
 };
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+        padding: 0,
+        margin: 0,
+        backgroundColor: '#0C2134',
     },
     dataContainer: {
+        backgroundColor: '#C2C1CC',
         marginVertical: 10,
         paddingHorizontal: 20,
     },
     dataTitle: {
         fontSize: 18,
         fontWeight: 'bold',
+        color:'orange'
+       
     },
     eventoText: {
         fontSize: 16,
     },
     noDataContainer: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        marginTop: 20,
+        color: '#C2C1CC',
+        fontSize: 20,
     },
 });
 export default DadosSalvos;
